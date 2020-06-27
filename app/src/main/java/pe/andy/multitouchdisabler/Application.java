eb6b8bd0fb98c3f295476185e8b8e2a049fc87ea
Andy <strawberryit@daum.net>
2020-06-28 02:46:35 +0900

    Disable left/right key repeat

--- app/src/main/java/pe/andy/multitouchdisabler/Application.java
+++ app/src/main/java/pe/andy/multitouchdisabler/Application.java
@@ -3,6 +3,7 @@ package pe.andy.multitouchdisabler;
 import android.app.Activity;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -10,6 +11,8 @@ import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
+import static android.view.KeyEvent.KEYCODE_PAGE_DOWN;
+import static android.view.KeyEvent.KEYCODE_PAGE_UP;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
 public class Application implements IXposedHookLoadPackage {
@@ -28,6 +31,12 @@ public class Application implements IXposedHookLoadPackage {
                         "dispatchTouchEvent",
                         MotionEvent.class,
                         ignoreMultitouch);
+
+                findAndHookMethod(
+                        clazz,
+                        "dispatchKeyEvent",
+                        KeyEvent.class,
+                        ignoreKeyRepeat);
             }
         }
         catch (Exception e) {
@@ -43,6 +52,13 @@ public class Application implements IXposedHookLoadPackage {
                         "dispatchTouchEvent",
                         MotionEvent.class,
                         ignoreMultitouch);
+
+                findAndHookMethod(
+                        "com.ridi.books.viewer.reader.activity.b",
+                        lpparam.classLoader,
+                        "dispatchKeyEvent",
+                        KeyEvent.class,
+                        ignoreKeyRepeat);
             }
         }
         catch (Exception e) {
@@ -67,6 +83,33 @@ public class Application implements IXposedHookLoadPackage {
                 ex.printStackTrace();
             }
             super.beforeHookedMethod(param);
+        }
+    };
+
+    final XC_MethodHook ignoreKeyRepeat = new XC_MethodHook() {
+        // Method signature
+        // public boolean dispatchKeyEvent(KeyEvent event)
+
+        @Override
+        protected void beforeHookedMethod(MethodHookParam param) {
+            try {
+                KeyEvent event = (KeyEvent) param.args[0];
+
+                if (isLeftRightKeyRepeat(event)) {
+                    // handled the event
+                    param.setResult(true);
+                }
+            }
+            catch (Exception ex) {
+                Log.d("MultiTouchDisabler", "dispatchKeyEvent hooking failed.");
+                ex.printStackTrace();
+            }
+        }
+
+        boolean isLeftRightKeyRepeat(KeyEvent event) {
+            return (event.getKeyCode() == KEYCODE_PAGE_UP ||   // LEFT
+                    event.getKeyCode() == KEYCODE_PAGE_DOWN)  // RIGHT
+                && event.getRepeatCount() > 0;
         }
     };
 }
